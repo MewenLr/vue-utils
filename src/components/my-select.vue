0d42c143253dc9843ca7e3ce45faccba1e5577f1
Mewen <mewen.leroux@edenred.com>
2020-03-31 17:42:28 +0200

    Add next / previous options

--- src/components/my-select.vue
+++ src/components/my-select.vue
@@ -8,6 +8,8 @@
       @blur.prevent="closeList"
       @focus.prevent="openList"
       @keyup.esc.prevent="blurInput"
+      @keydown.down.prevent="nextOption"
+      @keydown.up.prevent="previousOption"
       @input="searchOption($event.target.value)"
     )
     button.my-select_button(
@@ -18,8 +20,10 @@
       v-show="open"
     )
       li.my-select_list_option(
-        v-for="option in compListOptions"
+        v-for="(option, index) in compListOptions"
         :key="option.id"
+        :class="{ 'my-select_list_option--hover': target === index }"
+        @mouseenter="target = index"
         @mousedown="selectOption(option)"
       )
         | {{ option[label] }}
@@ -37,12 +41,17 @@ export default {
   data: () => ({
     open: false,
     inputValue: '',
+    target: undefined,
     searchedOptions: null,
     cancelMsg: 'Annuler la sélection',
+    notFoundMsg: 'Aucune option trouvée',
   }),
   computed: {
     compListOptions() {
-      if (this.searchedOptions) return this.searchedOptions
+      if (this.searchedOptions) {
+        if (!this.searchedOptions.length) return [{ [this.label]: this.notFoundMsg }]
+        return this.searchedOptions
+      }
       if (this.clearable) return [{ [this.label]: this.cancelMsg }, ...this.options]
       return this.options
     },
@@ -72,6 +81,7 @@ export default {
     closeList() {
       console.log('here in close')
       this.open = false
+      this.taget = undefined
       this.searchedOptions = null
       this.$refs.selectInput.value = this.inputValue
     },
@@ -85,6 +95,13 @@ export default {
       const re = new RegExp(`^(${value})`, 'gi')
       this.searchedOptions = this.options.filter((opt) => (opt[this.label].match(re) ? opt : false)).map((opt) => opt)
       if (this.searchedOptions.length === this.options.length) this.searchedOptions = null
+    },
+    nextOption() {
+      if (this.target === undefined) this.target = 0
+      else if (this.target < (this.compListOptions.length - 1)) this.target += 1
+    },
+    previousOption() {
+      if (this.target > 0) this.target -= 1
     },
   },
 }
@@ -109,13 +126,13 @@ export default {
   &_button
     top: 0
     right: 0
-    position: absolute
     color: #777
     width: 40px
     height: 100%
     border: none
     outline: none
     cursor: inherit
+    position: absolute
     background-color: transparent
     transition: transform linear .1s
 
@@ -138,9 +155,10 @@ export default {
     width: 100%
     overflow: auto
     padding: 10px 0
+    min-height: 36px
     max-height: 250px
     position: absolute
-    border-radius: 10px
+    border-radius: 5px
     top: calc(100% + 10px)
     background-color: #fff
     box-shadow: 0 0 10px 0 #b2b2b2
